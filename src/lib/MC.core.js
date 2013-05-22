8f88de8b9c72a748b18d49a582c41d0b157529ba
angel <angel.lai.cat@gmail.com>
2013-05-22 12:57:15 +0800

    MC.core optimize

--- src/lib/MC.core.js
+++ src/lib/MC.core.js
@@ -1,3 +1,12 @@
+/*
+#**********************************************************
+#* Filename: MC.core.js
+#* Creator: Angel
+#* Description: The core of the whole system 
+# **********************************************************
+# (c)Copyright 2013 Madeiracloud  All Rights Reserved
+# **********************************************************
+*/
 var MC = {
 	version: '0.1',
 
@@ -326,60 +335,7 @@ $(document).ready(function ()
  *
  * @example var json = $.xml2json(response);
  */
-(function(){
-	jQuery.extend({
-
-		/**
-		 * Converts an xml response object from a $.ajax() call to a JSON object.
-		 *
-		 * @param xml
-		 */
-		xml2json: function xml2json(xml)
-		{
-			var result = {};
-
-			for (var i in xml.childNodes)
-			{
-				var node = xml.childNodes[i];
-				if (node.nodeType == 1)
-				{
-					var child = node.hasChildNodes() ? xml2json(node) : node.nodevalue;
-					child = child == null ? {} : child;
-
-					if (result.hasOwnProperty(node.nodeName)) {
-						// For repeating elements, cast the node to array
-						if(!(result[node.nodeName] instanceof Array)){
-							var tmp = result[node.nodeName];
-							result[node.nodeName] = [];
-							result[node.nodeName].push(tmp);
-						}
-						result[node.nodeName].push(child);
-					}
-					else
-					{
-						result[node.nodeName] = child;
-					}
-
-					// Add attributes if any
-					if(node.attributes.length > 0) {
-						result[node.nodeName]['@attributes'] = {};
-						for(var j in node.attributes) {
-							var attribute = node.attributes.item(j);
-							result[node.nodeName]['@attributes'][attribute.nodeName] = attribute.nodeValue;
-						}
-					}
-
-					// Add element value
-					if(node.childElementCount == 0 && node.textContent != null && node.textContent != "") {
-						result[node.nodeName]/*.value */= node.textContent.trim();
-					}
-				}
-			}
-
-			return result;
-		}
-	});
-})();
+(function(){jQuery.extend({xml2json:function f(d){var b={},e;for(e in d.childNodes){var a=d.childNodes[e];if(1==a.nodeType){var c=a.hasChildNodes()?f(a):a.nodevalue,c=null==c?{}:c;if(b.hasOwnProperty(a.nodeName)){if(!(b[a.nodeName]instanceof Array)){var g=b[a.nodeName];b[a.nodeName]=[];b[a.nodeName].push(g)}b[a.nodeName].push(c)}else b[a.nodeName]=c;if(0<a.attributes.length){b[a.nodeName]["@attributes"]={};for(var h in a.attributes)c=a.attributes.item(h),b[a.nodeName]["@attributes"][c.nodeName]=c.nodeValue}0==a.childElementCount&&(null!=a.textContent&&""!=a.textContent)&&(b[a.nodeName]=a.textContent.trim())}}return b}})})();
 
 /*!
  * jQuery Cookie Plugin v1.3.1
@@ -388,86 +344,11 @@ $(document).ready(function ()
  * Copyright 2013 Klaus Hartl
  * Released under the MIT license
  */
-(function ($) {
+(function(e){function m(a){return a}function n(a){return decodeURIComponent(a.replace(j," "))}function k(a){0===a.indexOf('"')&&(a=a.slice(1,-1).replace(/\\"/g,'"').replace(/\\\\/g,"\\"));try{return d.json?JSON.parse(a):a}catch(c){}}var j=/\+/g,d=e.cookie=function(a,c,b){if(void 0!==c){b=e.extend({},d.defaults,b);if("number"===typeof b.expires){var g=b.expires,f=b.expires=new Date;f.setDate(f.getDate()+g)}c=d.json?JSON.stringify(c):String(c);return document.cookie=[d.raw?a:encodeURIComponent(a),"=",d.raw?c:encodeURIComponent(c),b.expires?"; expires="+b.expires.toUTCString():"",b.path?"; path="+b.path:"",b.domain?"; domain="+b.domain:"",b.secure?"; secure":""].join("")}c=d.raw?m:n;b=document.cookie.split("; ");for(var g=a?void 0:{},f=0,j=b.length;f<j;f++){var h=b[f].split("="),l=c(h.shift()),h=c(h.join("="));if(a&&a===l){g=k(h);break}a||(g[l]=k(h))}return g};d.defaults={};e.removeCookie=function(a,c){return void 0!==e.cookie(a)?(e.cookie(a,"",e.extend({},c,{expires:-1})),!0):!1}})(jQuery);
 
-	var pluses = /\+/g;
-
-	function raw(s) {
-		return s;
-	}
-
-	function decoded(s) {
-		return decodeURIComponent(s.replace(pluses, ' '));
-	}
-
-	function converted(s) {
-		if (s.indexOf('"') === 0) {
-			// This is a quoted cookie as according to RFC2068, unescape
-			s = s.slice(1, -1).replace(/\\"/g, '"').replace(/\\\\/g, '\\');
-		}
-		try {
-			return config.json ? JSON.parse(s) : s;
-		} catch(er) {}
-	}
-
-	var config = $.cookie = function (key, value, options) {
-
-		// write
-		if (value !== undefined) {
-			options = $.extend({}, config.defaults, options);
-
-			if (typeof options.expires === 'number') {
-				var days = options.expires, t = options.expires = new Date();
-				t.setDate(t.getDate() + days);
-			}
-
-			value = config.json ? JSON.stringify(value) : String(value);
-
-			return (document.cookie = [
-				config.raw ? key : encodeURIComponent(key),
-				'=',
-				config.raw ? value : encodeURIComponent(value),
-				options.expires ? '; expires=' + options.expires.toUTCString() : '', // use expires attribute, max-age is not supported by IE
-				options.path    ? '; path=' + options.path : '',
-				options.domain  ? '; domain=' + options.domain : '',
-				options.secure  ? '; secure' : ''
-			].join(''));
-		}
-
-		// read
-		var decode = config.raw ? raw : decoded;
-		var cookies = document.cookie.split('; ');
-		var result = key ? undefined : {};
-		for (var i = 0, l = cookies.length; i < l; i++) {
-			var parts = cookies[i].split('=');
-			var name = decode(parts.shift());
-			var cookie = decode(parts.join('='));
-
-			if (key && key === name) {
-				result = converted(cookie);
-				break;
-			}
-
-			if (!key) {
-				result[name] = converted(cookie);
-			}
-		}
-
-		return result;
-	};
-
-	config.defaults = {};
-
-	$.removeCookie = function (key, options) {
-		if ($.cookie(key) !== undefined) {
-			// Must not alter options, thus extending a fresh object...
-			$.cookie(key, '', $.extend({}, options, { expires: -1 }));
-			return true;
-		}
-		return false;
-	};
-})(jQuery);
-
+/*
+	Define as MC module
+ */
 if (typeof define === "function")
 {
 	define( "MC", [], function () { return MC; } );

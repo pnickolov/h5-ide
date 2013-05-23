7349ce85bb3584dee9a609cd8b2e314b1d0e48ca
Jimmy Xu <jimmy.xu@madeiracloud.com>
2013-05-24 00:54:04 +0800

    [develop] Rename gen_service.sh -> run.sh

---
+++ util/gen_service/readme.txt
@@ -0,0 +1,28 @@
+Description:
+generate js service from python api
+
+SOURCE_DIR(api.git)   : Source/INiT/Instant/Forge/AppService/
+TARGET_DIR(grunt.git) : util/gen_service/out.tmp/
+
+Usage:
+./run.sh
+
+############################################
+
+for example:
+
+
+[grunt.git]
+D:\Project\source\grunt
+
+[api.git]
+D:\Project\source\api
+
+[sh]
+D:\Project\source\grunt\util\gen_service\run.sh
+
+[source_dir]
+D:\Project\source\api\Source\INiT\Instant\Forge\AppService
+
+[target_dir]
+TARGET_DIR: D:\Project\source\grunt\util\gen_service\out.tmp
--- util/gen_service/run.sh
+++ util/gen_service/run.sh
@@ -33,48 +33,72 @@ TMPL_BASE_DIR=${SH_BASE_DIR}/"template"
 #===================================================================
 function fn_generate_coffee() {
 
-    CUR_DIR=$1
-    CUR_FILE=$2
-    TGT_DIR=$3
+    __TYPE=$1
+    __CUR_DIR=$2
+    __CUR_FILE=$3
+    __TGT_DIR=$4
 
     echo
     echo "#......................................................."
-    echo "# SRC_FILE: "${CUR_DIR}/${CUR_FILE}
-    echo "# TGT_DIR : "${TGT_DIR}
+    echo "# SRC_FILE: "${__CUR_DIR}/${__CUR_FILE}
+    echo "# TGT_DIR : "${__TGT_DIR}
     echo "#......................................................."
+
 
     API_NAME=()
     API_PARAM=()
 
-    #Resolve all api and params in ${CUR_DIR}/${CUR_FILE}
-    CMD=`cat ${CUR_DIR}/${CUR_FILE} | grep "def " | grep -v "def _" | grep -v "def test" | grep -v "#def" | grep -v "params_" | \
+    #Resolve all api and params in ${__CUR_DIR}/${__CUR_FILE}
+    __CMD=`cat ${__CUR_DIR}/${__CUR_FILE} | grep "def " | grep "self" | grep -v "def _" | grep -v "def test" | grep -v "#def" | grep -v "params_" | \
         awk 'BEGIN{FS="[ (:)]"}{printf "ORIGIN[%d]=\"%s\";API_NAME[%d]=\"%s\";",NR,$0,NR,$2; for ( x=4; x<=NF-2; x++){ gsub(",","",$x); printf "API_PARAM_%s[%s]=\"%s\";",NR,x-3,$x};printf "\n" }' `
     #Generate API_NAME and API_PARAM
-    eval ${CMD}
+    eval ${__CMD}
 
     ## generate coffee file ####################################################
 
-    TMP=${CUR_FILE/Handler/}    #remove Handler
+    TMP=${__CUR_FILE/Handler/}    #remove Handler
     _SERVICE_u=${TMP/.py/}      #remove .py -> eg: Session
     _SERVICE_l=${_SERVICE_u,,}     #tolower    -> eg: session
 
+    SERVICE_URL=""
+    API_TYPE=""
+    api_type=""
+    if [ "${__TYPE}" == "aws"  ]
+    then
+        SERVICE_URL="aws\/"${_SERVICE_l}
+        API_TYPE="AWS"
+        api_type="aws"
+    elif [ "${__TYPE}" == "awsutil"  ]
+    then
+        SERVICE_URL="aws"
+        API_TYPE="AWS"
+        api_type="aws"
+    else
+        SERVICE_URL=${_SERVICE_l}
+        API_TYPE="Forge"
+        api_type="forge"
+    fi
+
     echo "1.generate service.coffee (head)"
-    sed -e ":a;N;$ s/@@service-name/${_SERVICE_l}/g;ba" ${TMPL_BASE_DIR}/service.coffee.head \
+    sed -e ":a;N;$ s/@@service-name/${_SERVICE_l}/g;ba" ${TMPL_BASE_DIR}/service/service.coffee.head \
     | sed -e ":a;N;$ s/@@create-date/`date "+%Y-%m-%d %H:%M:%S"`/g;ba" \
-    > ${TGT_DIR}/${_SERVICE_l}_service.coffee
+    | sed -e ":a;N;$ s/@@service-url/${SERVICE_URL}/g;ba" \
+    | sed -e ":a;N;$ s/@@api-type/${api_type}/g;ba" \
+    > ${__TGT_DIR}/${_SERVICE_l}_service.coffee
 
     echo "2.generate parser.coffee (head)"
-    sed -e ":a;N;$ s/@@service-name/${_SERVICE_l}/g;ba" ${TMPL_BASE_DIR}/parser.coffee.head \
+    sed -e ":a;N;$ s/@@service-name/${_SERVICE_l}/g;ba" ${TMPL_BASE_DIR}/service/parser.coffee.head \
     | sed -e ":a;N;$ s/@@create-date/`date "+%Y-%m-%d %H:%M:%S"`/g;ba" \
-    > ${TGT_DIR}/${_SERVICE_l}_parser.coffee
+    > ${__TGT_DIR}/${_SERVICE_l}_parser.coffee
 
     echo "3.generate vo.coffee"
-    sed -e ":a;N;$ s/@@service-name/${_SERVICE_l}/g;ba" ${TMPL_BASE_DIR}/vo.coffee.tmpl \
+    sed -e ":a;N;$ s/@@service-name/${_SERVICE_l}/g;ba" ${TMPL_BASE_DIR}/service/vo.coffee.tmpl \
     | sed -e ":a;N;$ s/@@create-date/`date "+%Y-%m-%d %H:%M:%S"`/g;ba" \
-    > ${TGT_DIR}/${_SERVICE_l}_vo.coffee
+    > ${__TGT_DIR}/${_SERVICE_l}_vo.coffee
 
     echo "4.append api handler to service and parser"
     _PUBLIC_API_LIST=""
+    _PUBLIC_PARSER_LIST=""
     #loop by API_NAME
     for (( j = 1 ; j <= ${#API_NAME[@]} ; j++ ))
     do
@@ -89,7 +113,8 @@ function fn_generate_coffee() {
         _FUNC=`echo "${_CUR_API}" | awk '{len=split($0,a,"_");for (i=1;i<=len;i++){printf "%s%s",toupper(substr(a[i],0,1)),substr(a[i],2) } }'`
 
 
-        _PARAM_LIST=""
+        _PARAM_LIST_DEF=""  #in define
+        _PARAM_LIST=""      #in invoke
 
         P_NUM='echo ${#API_PARAM_'${j}'[@]}'
         P_NUM=`eval ${P_NUM}`
@@ -98,40 +123,76 @@ function fn_generate_coffee() {
         for (( k = 1 ; k <= ${P_NUM} ; k++ ))
         do
             TMP='${API_PARAM_'${j}'[$k]}'
+
             CUR_PARAM[$k]=`eval "echo $TMP" | awk 'BEGIN{FS="[=]"}{printf $1}' `
+            CUR_PARAM_DEF[$k]=`eval "echo $TMP" | sed "s/None/null/g" `
             #echo "    param> "${CUR_PARAM[$k]}
             if [ $k -eq 1 ]
             then
                 _PARAM_LIST=${CUR_PARAM[$k]}
+                _PARAM_LIST_DEF=${CUR_PARAM_DEF[$k]}
             else
                 _PARAM_LIST=${_PARAM_LIST}", "${CUR_PARAM[$k]}
+                _PARAM_LIST_DEF=${_PARAM_LIST_DEF}", "${CUR_PARAM_DEF[$k]}
             fi
         done
 
         #echo "CUR_PARAM: "${#CUR_PARAM[@]}
 
-        echo " > ${_CUR_API} "
+        NEED_RESOLVE=""
+        if [ "${__TYPE}" == "aws"  ]
+        then
+        #api in aws that Start with Describe/Get/List need resolve
+            NEED_RESOLVE=`echo "${_CUR_API}"  | grep -E "^Describe|^Get|^List" | wc -l `
+            if [ "${NEED_RESOLVE}" -eq 0  ]
+            then
+                NEED_RESOLVE=""
+                echo " > ${_CUR_API} "
+            else
+                NEED_RESOLVE=".resolve"
+                echo " > ${_CUR_API} (need resolve)"
+            fi
+        else
+        #all api in awsutil/forge/handler need resolve
+            NEED_RESOLVE=".resolve"
+            echo " > ${_CUR_API} (need resolve)"
+        fi
+
         #1.append api ( ${_CUR_API} ) to ${_SERVICE_l}_service.coffee
-        sed -e ":a;N;$ s/@@service-name/${_SERVICE_l}/g;ba" ${TMPL_BASE_DIR}/service.coffee.api \
+        sed -e ":a;N;$ s/@@service-name/${_SERVICE_l}/g;ba" ${TMPL_BASE_DIR}/service/service.coffee.api \
         | sed -e ":a;N;$ s/@@api-name/${_CUR_API}/g;ba" \
         | sed -e ":a;N;$ s/@@origin/${_CUR_ORIGIN}/g;ba" \
+        | sed -e ":a;N;$ s/@@param-list-def/${_PARAM_LIST_DEF}/g;ba" \
         | sed -e ":a;N;$ s/@@param-list/${_PARAM_LIST}/g;ba" \
         | sed -e ":a;N;$ s/@@parser-func/parser${_FUNC}Return/g;ba" \
-        >> ${TGT_DIR}/${_SERVICE_l}_service.coffee
+        >> ${__TGT_DIR}/${_SERVICE_l}_service.coffee
 
-        _PUBLIC_API_LIST="\tparser${_FUNC}Return \t: parser${_FUNC}Return\n"${_PUBLIC_API_LIST}
+        #public parser func
+        _TMP=`echo "parser${_FUNC}Return" | awk '{printf "    %-40s : %s\n",$0,$0}'`
+        _PUBLIC_PARSER_LIST=${_PUBLIC_PARSER_LIST}"\n"${_TMP}
+
+        #public api func
+        _TMP=`echo "${_CUR_API}" | awk '{printf "    %-28s : %s\n",$0,$0}'`
+        _PUBLIC_API_LIST=${_PUBLIC_API_LIST}"\n"${_TMP}
 
         #2.append api ( ${_CUR_API} ) to ${_SERVICE_l}_parser.coffee
-        sed -e ":a;N;$ s/@@service-name/${_SERVICE_l}/g;ba" ${TMPL_BASE_DIR}/parser.coffee.api \
+        sed -e ":a;N;$ s/@@service-name/${_SERVICE_l}/g;ba" ${TMPL_BASE_DIR}/service/parser.coffee.api${NEED_RESOLVE} \
         | sed -e ":a;N;$ s/@@api-name/${_CUR_API}/g;ba" \
         | sed -e ":a;N;$ s/@@parser-func/parser${_FUNC}Return/g;ba" \
-        | sed -e ":a;N;$ s/@@resolve-func/resolve${_FUNC}VO/g;ba" \
-        >> ${TGT_DIR}/${_SERVICE_l}_parser.coffee
+        | sed -e ":a;N;$ s/@@resolve-func/resolve${_FUNC}Result/g;ba" \
+        | sed -e ":a;N;$ s/@@api-type/${api_type}/g;ba" \
+        | sed -e ":a;N;$ s/@@API-TYPE/${API_TYPE}/g;ba" \
+        >> ${__TGT_DIR}/${_SERVICE_l}_parser.coffee
 
     done
 
-        echo "5.append public api list to ${_SERVICE_l}_service.coffee"
-        echo -e "${_PUBLIC_API_LIST}" >> ${TGT_DIR}/${_SERVICE_l}_service.coffee
+    echo "5.append public api list to ${_SERVICE_l}_service.coffee"
+    echo -e "\n    #############################################################\n\
+    #public ${_PUBLIC_API_LIST}" >> ${__TGT_DIR}/${_SERVICE_l}_service.coffee
+
+    echo "6.append public parser list to ${_SERVICE_l}_parser.coffee"
+    echo -e "\n    #############################################################\n\
+    #public ${_PUBLIC_PARSER_LIST}" >> ${__TGT_DIR}/${_SERVICE_l}_parser.coffee
 
     return
 
@@ -149,20 +210,25 @@ function fn_scan_handler_forge() {
     CUR_DIR=$1
     CUR_FILE=$2
 
-    TGT_DIR=${TGT_BASE_DIR}/"service"/${CUR_FILE/Handler/}    #remove Handler
-    TGT_DIR=${TGT_DIR/.py/}                         #remove .py
-    TGT_DIR=${TGT_DIR,,}                            #tolower
-
     #for tmp test
-    if [ "${CUR_FILE}" != "SessionHandler.py" ]
-    then
-        return
-    fi
+    #if [ "${CUR_FILE}" != "SessionHandler.py" ]
+    #then
+    #    return
+    #fi
+
+    echo "########################################################"
+    echo "#Processing "`echo ${CUR_DIR} | awk 'BEGIN{FS="[/]"}{print $(NF) }' `" - "${CUR_FILE}
+    echo "########################################################"
+
+
+    TGT_DIR=${TGT_BASE_DIR}/"service"/${CUR_FILE/Handler/}      #remove Handler
+    TGT_DIR=${TGT_DIR/.py/}                                     #remove .py
+    TGT_DIR=${TGT_DIR,,}                                        #tolower
 
     #create subdir in out.tmp
     mkdir -p ${TGT_DIR}
 
-    fn_generate_coffee "${CUR_DIR}" "${CUR_FILE}" "${TGT_DIR}"
+    fn_generate_coffee "forge" "${CUR_DIR}" "${CUR_FILE}" "${TGT_DIR}"
 
     return
 
@@ -172,24 +238,22 @@ function fn_scan_handler_forge() {
 # Scan python file for aws
 #===================================================================
 function fn_scan_aws() {
-#
-    return
 
     CUR_DIR=$1
     SERVICE=$2
     #echo $CUR_DIR
-
-    #service
-    echo
-    echo "#======================================================="
-    echo "#  SERVICE: ${SERVICE}"
-    echo "#======================================================="
 
     #for tmp test
     #if [ "${SERVICE}" != "EC2" ]
     #then
     #    return
     #fi
+
+    #service
+    echo
+    echo "#======================================================="
+    echo "#  SERVICE: ${SERVICE}"
+    echo "#======================================================="
 
     if [ "${SERVICE}" == "AWSUtil.py" ]
     then
@@ -198,11 +262,15 @@ function fn_scan_aws() {
         CUR_FILE=${SERVICE}
         SERVICE=${SERVICE/.py/} #remove .py
 
+        echo "########################################################"
+        echo "#Processing AWS - "${CUR_FILE}
+        echo "########################################################"
+
         TGT_DIR=${TGT_BASE_DIR}/"service"/aws/${SERVICE,,}
         #create subdir in out.tmp
         mkdir -p ${TGT_DIR}
 
-        fn_generate_coffee "${CUR_DIR}" "${CUR_FILE}" "${TGT_DIR}"
+        fn_generate_coffee "awsutil" "${CUR_DIR}" "${CUR_FILE}" "${TGT_DIR}"
 
     else
     #Except AWSUtil###################
@@ -212,6 +280,11 @@ function fn_scan_aws() {
         #resource
             CUR_FILE=${RESOURCE}
             RESOURCE=${RESOURCE/.py/}
+
+            echo "########################################################"
+            echo "#Processing AWS - "${SERVICE}" - "${CUR_FILE}
+            echo "########################################################"
+
             echo
             echo "#-------------------------------------------------------"
             echo "#   RESOURCE: ${RESOURCE}"
@@ -221,7 +294,7 @@ function fn_scan_aws() {
             #create subdir in out.tmp
             mkdir -p ${TGT_DIR}
 
-            fn_resolve_api "${CUR_DIR}/${SERVICE}" "${CUR_FILE}" "${TGT_DIR}"
+            fn_generate_coffee "aws" "${CUR_DIR}/${SERVICE}" "${CUR_FILE}" "${TGT_DIR}"
         done
 
     fi
@@ -239,10 +312,6 @@ do
 
     for LINE in `ls ${CUR_SRC_DIR} | grep -v "__init__" | grep -v "View" | grep -v "EventHandler"`
     do
-        echo
-        echo "########################################################"
-        echo "#Processing "${SRC_DIR[$i]}
-        echo "########################################################"
         if [ "${SRC_DIR[$i]}" == "Handler" -o "${SRC_DIR[$i]}" == "Forge" ]
         then
             fn_scan_handler_forge "${CUR_SRC_DIR}" "${LINE}"

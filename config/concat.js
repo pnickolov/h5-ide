5e6f945f555ecab352973bf944cddd3d7e4175ef
Kenshin <fxblog@gmail.com>
2013-06-07 17:16:05 +0800

    modify grunt concat task

--- config/concat.js
+++ config/concat.js
@@ -2,7 +2,7 @@
 module.exports = {
 
 	meteor: {
-		src: [ './meteor/underscore.js','./meteor/client_environment.js','./meteor/helpers.js','./meteor/timers.js','./meteor/errors.js','./meteor/fiber_stubs_client.js','./meteor/dynamics_browser.js','./meteor/deps.js','./meteor/reactive-dict.js','./meteor/session.js','./meteor/random.js','./meteor/ejson.js','./meteor/base64.js','./meteor/sockjs-0.3.4.js','./meteor/stream_client_sockjs.js','./meteor/stream_client_common.js','./meteor/minimongo.js','./meteor/selector.js','./meteor/modify.js','./meteor/diff.js','./meteor/objectid.js','./meteor/livedata_common.js','./meteor/livedata_connection.js','./meteor/client_convenience.js','./meteor/local_collection_driver.js','./meteor/collection.js','./meteor/startup_client.js','./meteor/json2.js' ],
+		src: [ './meteor/client_environment.js','./meteor/helpers.js','./meteor/timers.js','./meteor/errors.js','./meteor/fiber_stubs_client.js','./meteor/dynamics_browser.js','./meteor/deps.js','./meteor/reactive-dict.js','./meteor/session.js','./meteor/random.js','./meteor/ejson.js','./meteor/sockjs-0.3.4.js','./meteor/stream_client_sockjs.js','./meteor/stream_client_common.js','./meteor/minimongo.js','./meteor/selector.js','./meteor/modify.js','./meteor/diff.js','./meteor/objectid.js','./meteor/livedata_common.js','./meteor/livedata_connection.js','./meteor/client_convenience.js','./meteor/local_collection_driver.js','./meteor/collection.js','./meteor/startup_client.js' ],
 		dest: './meteor/meteor.js'
 	}
 

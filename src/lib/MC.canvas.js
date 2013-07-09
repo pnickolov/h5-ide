f2cb60a6a322393b6ccf888d5c61930b6f96ad86
angel <angel.lai.cat@gmail.com>
2013-07-09 14:11:01 +0800

    Fix zoomIn/Out scale-ratio calculation

--- src/lib/MC.canvas.js
+++ src/lib/MC.canvas.js
@@ -14,9 +14,7 @@ MC.canvas = {
 
 		if (MC.canvas.SCALE_RATIO < 1)
 		{
-			MC.canvas.SCALE_RATIO -= 0.2;
-
-			MC.canvas.SCALE_RATIO = Math.round(MC.canvas.SCALE_RATIO);
+			MC.canvas.SCALE_RATIO = (MC.canvas.SCALE_RATIO * 10 + 2) / 10;
 
 			$('#svg_canvas')[0].setAttribute('viewBox', '0 0 ' + MC.canvas.GRID_WIDTH * canvas_size[0] * MC.canvas.SCALE_RATIO + ' ' + MC.canvas.GRID_HEIGHT * canvas_size[1] * MC.canvas.SCALE_RATIO);
 
@@ -30,9 +28,7 @@ MC.canvas = {
 
 		if (MC.canvas.SCALE_RATIO < 1.6)
 		{
-			MC.canvas.SCALE_RATIO += 0.2;
-
-			MC.canvas.SCALE_RATIO = Math.round(MC.canvas.SCALE_RATIO);
+			MC.canvas.SCALE_RATIO = (MC.canvas.SCALE_RATIO * 10 + 2) / 10;
 			
 			$('#svg_canvas')[0].setAttribute('viewBox', '0 0 ' + MC.canvas.GRID_WIDTH * canvas_size[0] * MC.canvas.SCALE_RATIO + ' ' + MC.canvas.GRID_HEIGHT * canvas_size[1] * MC.canvas.SCALE_RATIO);
 

cc0b42bab971052477e5bb7e4cc39abd856b05f8
song <imeoer@gmail.com>
2013-07-25 14:47:42 +0800

    del some js in src/aws

--- src/lib/aws/main.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  define(['MC', 'lib/aws/elb/elb', 'lib/aws/vpn/vpn'], function(MC, aws_handle_elb, aws_handle_vpn) {
-    return MC.aws = {
-      elb: aws_handle_elb,
-      vpn: aws_handle_vpn
-    };
-  });
-
-}).call(this);

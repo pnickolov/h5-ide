f9355890eff9f77fdb74da647570bb57dfb5550b
Jimmy Xu <jimmy.xu@madeiracloud.com>
2013-08-02 10:18:58 +0800

    [feature/module-canvas-v3] Change websocket server port to 8300

--- src/lib/MC.core.js
+++ src/lib/MC.core.js
@@ -14,7 +14,7 @@ var MC = {
 	// Global Variable
 	API_URL: 'https://api.madeiracloud.com/',
 	IMG_URL: '../assets/images/',
-	WS_URL: 'http://api.madeiracloud.com:3000',//-> 8300
+	WS_URL: 'http://api.madeiracloud.com:8300',//-> 8300
 	SAVEPNG_URL: 'http://api.madeiracloud.com:8320/savepng',
 
 	// Global data

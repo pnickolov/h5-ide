c6a5dbf6dd666ef6e71ec89db20a13f3b356b41a
angel <angel.lai.cat@gmail.com>
2013-08-23 11:47:10 +0800

    [feature/module-canvas-v3] UI component update

--- src/ui/common/UI.bubble.js
+++ src/ui/common/UI.bubble.js
@@ -3,83 +3,87 @@
 #* Filename: UI.bubble
 #* Creator: Angel
 #* Description: UI.bubble
-#* Date: 20130812
+#* Date: 20130823
 # **********************************************************
 # (c) Copyright 2013 Madeiracloud  All Rights Reserved
 # **********************************************************
 */
-var bubble = function (event)
+
+(function ()
 {
-	if (event.type === 'mouseleave')
+	var bubble = function (event)
 	{
-		bubble.clear();
-
-		return false;
-	}
-
-	var target = $(this),
-		content = target.data('bubble-template'),
-		data = target.data('bubble-data'),
-		bubble_box = $('#bubble-box'),
-		coordinate = {},
-		width,
-		height,
-		target_offset,
-		target_width,
-		target_height;
-
-	if ($.trim(content) !== '')
-	{
-		if (!bubble_box[0])
-		{
-			$(document.body).append('<div id="bubble-box"><div class="arrow"></div><div id="bubble-content"></div></div>');
-			bubble_box = $('#bubble-box');
-		}
-
-		$('#bubble-content').html(
-			MC.template[ content ]( data )
-		);
-
-		target_offset = target.offset();
-		target_width = target.innerWidth();
-		target_height = target.innerHeight();
-
-		width = bubble_box.width();
-		height = bubble_box.height();
-
-		if (target_offset.left + target_width + width - document.body.scrollLeft > window.innerWidth)
-		{
-			coordinate.left = target_offset.left - width - 15;
-			bubble_box.addClass('bubble-right');
-		}
-		else
-		{
-			coordinate.left = target_offset.left + target_width + 15;
-			bubble_box.addClass('bubble-left');
-		}
-
-		coordinate.top = target_offset.top - ((height - target_height) / 2);
-
-		bubble_box.css(coordinate).show();
-	}
-
-	bubble.timer = setInterval(function ()
-	{
-		if (target.closest('html').length === 0)
+		if (event.type === 'mouseleave')
 		{
 			bubble.clear();
+
+			return false;
 		}
-	}, 1000);
-};
 
-bubble.clear = function ()
-{
-	$('#bubble-box').remove();
+		var target = $(this),
+			content = target.data('bubble-template'),
+			data = target.data('bubble-data'),
+			bubble_box = $('#bubble-box'),
+			coordinate = {},
+			width,
+			height,
+			target_offset,
+			target_width,
+			target_height;
 
-	clearInterval(bubble.timer);
-};
+		if ($.trim(content) !== '')
+		{
+			if (!bubble_box[0])
+			{
+				$(document.body).append('<div id="bubble-box"><div class="arrow"></div><div id="bubble-content"></div></div>');
+				bubble_box = $('#bubble-box');
+			}
 
-$(document).ready(function ()
-{
-	$(document.body).on('mouseenter mouseleave', '.bubble', bubble);
-});
+			$('#bubble-content').html(
+				MC.template[ content ]( data )
+			);
+
+			target_offset = target.offset();
+			target_width = target.innerWidth();
+			target_height = target.innerHeight();
+
+			width = bubble_box.width();
+			height = bubble_box.height();
+
+			if (target_offset.left + target_width + width - document.body.scrollLeft > window.innerWidth)
+			{
+				coordinate.left = target_offset.left - width - 15;
+				bubble_box.addClass('bubble-right');
+			}
+			else
+			{
+				coordinate.left = target_offset.left + target_width + 15;
+				bubble_box.addClass('bubble-left');
+			}
+
+			coordinate.top = target_offset.top - ((height - target_height) / 2);
+
+			bubble_box.css(coordinate).show();
+		}
+
+		bubble.timer = setInterval(function ()
+		{
+			if (target.closest('html').length === 0)
+			{
+				bubble.clear();
+			}
+		}, 1000);
+	};
+
+	bubble.clear = function ()
+	{
+		$('#bubble-box').remove();
+
+		clearInterval(bubble.timer);
+	};
+
+	$(document).ready(function ()
+	{
+		$(document.body).on('mouseenter mouseleave', '.bubble', bubble);
+	});
+})();
--- src/ui/common/UI.tooltip.js
+++ src/ui/common/UI.tooltip.js
@@ -3,79 +3,83 @@
 #* Filename: UI.tooltip
 #* Creator: Angel
 #* Description: UI.tooltip
-#* Date: 20130817
+#* Date: 20130823
 # **********************************************************
 # (c) Copyright 2013 Madeiracloud  All Rights Reserved
 # **********************************************************
 */
-var tooltip = function (event)
-{
-	var target = $(this),
-		content = $.trim(target.data('tooltip')),
-		tooltip_box = $('#tooltip_box'),
-		target_offset,
-		width,
-		height,
-		target_width,
-		target_height,
-		tooltip_timer;
 
-	if (content !== '')
+(function ()
+{
+	var tooltip = function (event)
 	{
-		if (!tooltip_box[0])
+		var target = $(this),
+			content = $.trim(target.data('tooltip')),
+			tooltip_box = $('#tooltip_box'),
+			target_offset,
+			width,
+			height,
+			target_width,
+			target_height,
+			tooltip_timer;
+
+		if (content !== '')
 		{
-			$(document.body).append('<div id="tooltip_box"></div>');
-			tooltip_box = $('#tooltip_box');
-		}
-
-		tooltip_box.text(content);
-
-		if (target.prop('namespaceURI') === 'http://www.w3.org/2000/svg')
-		{
-			target_offset = target[0].getBoundingClientRect();
-			target_width = target_offset.width;
-			target_height = target_offset.height;
-		}
-		else
-		{
-			target_offset = target.offset();
-			target_width = target.innerWidth();
-			target_height = target.innerHeight();
-		}
-
-		width = tooltip_box.width();
-		height = tooltip_box.height();
-		
-		tooltip_box.css({
-			'left': target_offset.left + width - document.body.scrollLeft > window.innerWidth ?
-				target_offset.left - width :
-				target_offset.left + 5,
-			'top': target_offset.top + target_height + height - document.body.scrollTop + 45 > window.innerHeight ?
-				target_offset.top - height - 15 :
-				target_offset.top + target_height + 8
-		}).show();
-
-		$(document.body).on('mouseleave', '.tooltip', tooltip.clear);
-
-		tooltip.timer = setInterval(function ()
-		{
-			if (target.closest('html').length === 0)
+			if (!tooltip_box[0])
 			{
-				tooltip.clear();
+				$(document.body).append('<div id="tooltip_box"></div>');
+				tooltip_box = $('#tooltip_box');
 			}
-		}, 1000);
-	}
-};
 
-tooltip.clear = function ()
-{
-	$('#tooltip_box').hide();
-	$(document.body).off('mouseleave', '.tooltip', tooltip.clear);
+			tooltip_box.text(content);
 
-	clearInterval(tooltip.timer);
-};
+			if (target.prop('namespaceURI') === 'http://www.w3.org/2000/svg')
+			{
+				target_offset = target[0].getBoundingClientRect();
+				target_width = target_offset.width;
+				target_height = target_offset.height;
+			}
+			else
+			{
+				target_offset = target.offset();
+				target_width = target.innerWidth();
+				target_height = target.innerHeight();
+			}
 
-$(document).ready(function ()
-{
-	$(document.body).on('mouseenter', '.tooltip', tooltip);
-});
+			width = tooltip_box.width();
+			height = tooltip_box.height();
+			
+			tooltip_box.css({
+				'left': target_offset.left + width - document.body.scrollLeft > window.innerWidth ?
+					target_offset.left - width :
+					target_offset.left + 5,
+				'top': target_offset.top + target_height + height - document.body.scrollTop + 45 > window.innerHeight ?
+					target_offset.top - height - 15 :
+					target_offset.top + target_height + 8
+			}).show();
+
+			$(document.body).on('mouseleave', '.tooltip', tooltip.clear);
+
+			tooltip.timer = setInterval(function ()
+			{
+				if (target.closest('html').length === 0)
+				{
+					tooltip.clear();
+				}
+			}, 1000);
+		}
+	};
+
+	tooltip.clear = function ()
+	{
+		$('#tooltip_box').hide();
+		$(document.body).off('mouseleave', '.tooltip', tooltip.clear);
+
+		clearInterval(tooltip.timer);
+	};
+
+	$(document).ready(function ()
+	{
+		$(document.body).on('mouseenter', '.tooltip', tooltip);
+	});
+})();

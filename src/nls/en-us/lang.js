5301f227dceb162a25ce340dc5ba7d5cc282c17d
danoprey <dan@oprey.co.uk>
2013-08-29 18:26:26 +0800

    canvas resize

--- src/nls/en-us/lang.js
+++ src/nls/en-us/lang.js
@@ -109,8 +109,11 @@ define({
 		'CVS_CFM_DEL_ASG'                       : 'Deleting this will delete the entire %s. Are you sure you want to delete it?',
 		'CVS_CFM_ADD_IGW'                       : 'An Internet Gateway is Required',
 		'CVS_CFM_ADD_IGW_MSG'                   : 'Automatically add an internet gateway to allow this %s to be publicly addressable?',
+		'CVS_TIP_EXPAND_W'   					: 'Increase Canvas Width',
+		'CVS_TIP_SHRINK_W'   					: 'Decrease Canvas Width',
+		'CVS_TIP_EXPAND_H'   					: 'Increase Canvas Height',
+		'CVS_TIP_SHRINK_H'   					: 'Decrease Canvas Height',
 		// Add new strings below this comment. Move above once English has been confirmed
-
 
 		/******property module******/
 		// instance property module

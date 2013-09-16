6f9a22aa5a247c08f55d63b764c3f1942746cda9
TimZhang <timzhang@dnspod.com>
2013-09-16 14:55:38 +0800

    [release/v0.3-201309]: Test-Console: modify gitignore and add MC.template.js

---
+++ src/test/madeira_console/lib/MC.template.js
@@ -0,0 +1,50 @@
+/*
+#**********************************************************
+#* Filename: MC.template.js
+#* Creator: Angel
+#* Description: The file to storage HTML templates
+#* Date: 20130803
+# **********************************************************
+# (c) Copyright 2013 Madeiracloud  All Rights Reserved
+# **********************************************************
+*/
+
+var MC = MC || {},
+	TEMPLATE_LOCATION = '../../ui/common/MC.template.html';
+
+$.get(TEMPLATE_LOCATION, function (data)
+{
+	var data = data.split(/\<!-- (.*) --\>/ig),
+		template = {},
+		i = 1,
+		l = data.length,
+		space_labels,
+		space,
+		label_length;
+
+	for (; i < l; i += 2)
+	{
+		space = template;
+		space_labels = data[ i ].split('.');
+		label_length = space_labels.length - 1;
+
+		$.each(space_labels, function (index, value)
+		{
+			if (!space[ value ])
+			{
+				space[ value ] = {};
+			}
+
+			if (label_length === index)
+			{
+				space[ value ] = Handlebars.compile(data[ i + 1 ].trim());
+			}
+			else
+			{
+				space = space[ value ];
+			}
+		});
+	}
+
+	MC.template = template;
+});

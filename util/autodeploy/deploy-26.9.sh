1d859b0b0b78469e79487b57c16d132d58d5d585
Jimmy Xu <jimmy.xu@madeiracloud.com>
2014-01-12 01:25:54 +0800

    [feature/state-editor] Add deploy-26.9.sh

---
+++ util/autodeploy/deploy-26.9.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+DIR=$(cd "$(dirname "$0")"; pwd)
+cd $DIR
+
+FILENAME="ide.tar.gz"
+
+cd ${DIR}/../../src
+
+echo "now will deploy develop version to 26.9"
+echo "start make(grunt debug)..."
+grunt debug
+echo "make done"
+echo
+
+cd ${DIR}/../../debug
+
+COMMIT=`git log -n 1 | grep commit | awk '{print $2}' `
+AUTHOR=`git config --list | grep "user.name" | awk  'BEGIN{FS="="}{print $2}'`
+echo ">generate commit id: ${COMMIT}"
+echo -e "${COMMIT:0:3}.dev\n${AUTHOR}" > commit
+echo ""
+
+
+rm ${FILENAME} -rf
+echo "start tar..."
+tar czf ${FILENAME} *
+echo "tar done."
+
+echo "start transfer..."
+scp ${FILENAME} root@211.98.26.9:/madeira/site/temp
+echo "transfer done."
+echo
+echo "${FILENAME} had been uploaded to /madeira/site/temp of 26.9"
+echo "ide on 26.9 will be updated after severial minutes."
+echo
+echo "remove ${FILENAME}"
+rm ${FILENAME} -rf
+rm commit
+echo
+echo "all done. "

adda4e052d93f73ca6c950c1963034c1d3d5c37a
TimZhang <tim@mc2.io>
2014-01-17 19:33:40 +0800

    [hotfix/parsley-update-tim] Upgrade parsley

--- src/js/ide/config.coffee
+++ src/js/ide/config.coffee
@@ -86,6 +86,7 @@ require.config {
 		'UI.canvg'         : 'ui/common/UI.canvg'
 		'UI.sortable'      : 'ui/common/jquery.sortable'
 		'UI.parsley'       : 'ui/common/UI.parsley'
+		'UI.errortip'      : 'ui/common/UI.errortip'
 
 		#jquery plugin
 		'hoverIntent'        : 'ui/common/jquery.hoverIntent.minified'
@@ -335,7 +336,10 @@ require.config {
 			deps       : [ 'jquery' ]
 
 		'UI.parsley'   :
-			deps       : [ 'jquery' ]
+            deps       : [ 'jquery', 'UI.errortip' ]
+
+        'UI.errortip'   :
+            deps       : [ 'jquery' ]
 
 		'bootstrap-carousel':
 			deps	   : [ 'jquery' ]
--- src/ui/common/UI.parsley.js
+++ src/ui/common/UI.parsley.js
@@ -614,7 +614,8 @@
             ipaddress: '^[0-9]*$|^[0-9][0-9./]+$',
             domain: '^[a-zA-Z0-9]+[a-zA-Z0-9.-]*$',
             digits: '^[0-9]*$',
-            ascii: '^[\x00-\x7F]*$'
+            ascii: '^[\x00-\x7F]*$',
+            number: '^-?[0-9]*$|^-?[0-9]+\\.[0-9]*$'
           };
 
           vlidateType = this.options.type;
@@ -626,7 +627,7 @@
           var delayHandlerFactory = function(origin, context, times) {
             var getResult = function(val) {
               return that.Validator.validators[ 'regexp' ]( val, regExp, that );
-            }
+            };
 
             return function( e ) {
                 // run specify times
@@ -643,35 +644,29 @@
                 if ( !result ) {
                   $( context ).val( origin ).parsley( 'validate' );
                 }
-              }
+              };
 
-            }
-
-          // Disable context menu
-          this.$element.on('contextmenu', function( e ) {
-            return false;
-          });
-
-          // Handle drag
-          this.$element.on('dragenter', function( e ) {
-            var origin = $( this ).val();
-            var delayHandler = delayHandlerFactory( origin, this, 1 )
-            $(this).one( 'mouseup mousedown keydown keyup blur', delayHandler );
-          });
-
-
-          // Handle keydown
-          var keydownHandler = function(e) {
-            var origin = $( this ).val();
-            var delayHandler = Util.runOnceInSametime( delayHandlerFactory( origin, this, 1 ) );
-            $(this).one( 'keyup blur', delayHandler );
           };
 
+          // Handle paste and drop
+          this.$element.on( 'drop paste', function( e ) {
+            var origin = $( this ).val();
+            var delayHandler = delayHandlerFactory( origin, this, 1 );
+            setTimeout( delayHandler, 1 );
+          });
 
-          this.$element.on( 'keydown', keydownHandler);
+          // Handle Chinese
+          var keydownHandler = function(e) {
+            if ( e.which === 229 ) {
+              var origin = $( this ).val();
+              var delayHandler = delayHandlerFactory( origin, this, 1 );
+              $( this ).one( 'keyup', delayHandler );
+              return false;
+            }
+          };
 
           // Handle keypress( main )
-          var keypressHandler = function(e) {
+          var ignoreHandler = function(e) {
             var inputChar, isLegal;
             // control key green light
             if (e.which in controlCodeList) return true;
@@ -687,7 +682,8 @@
             if ( !isLegal ) return false;
           };
 
-          this.$element.on( 'keypress', keypressHandler);
+          this.$element.on( 'keypress', ignoreHandler );
+          this.$element.on( 'keydown', keydownHandler );
 
         }
 
@@ -1121,11 +1117,11 @@
       if ( true === this.valid ) {
         this.removeErrors();
         this.errorClassHandler.removeClass( this.options.errorClass ).addClass( this.options.successClass );
-        Util.errortip.clear( this.hash );
+        errortip.purge( this.hash );
         return true;
       } else if ( false === this.valid ) {
         this.errorClassHandler.removeClass( this.options.successClass ).addClass( this.options.errorClass );
-        Util.errortip.first( this.element );
+        errortip.first( this.element );
         return false;
       }
 
@@ -1705,7 +1701,7 @@
   */
   $.fn.parsley.defaults = {
     // basic data-api overridable properties here..
-    inputs: 'input, textarea, select'           // Default supported inputs.
+    inputs: 'input, textarea, select, [contenteditable="true"]'           // Default supported inputs.
     , excluded: 'input[type=hidden], input[type=file], :disabled' // Do not validate input[type=hidden] & :disabled.
     , trigger: false                            // $.Event() that will trigger validation. eg: keyup, change..
     , animate: false                             // fade in / fade out error messages
@@ -1753,7 +1749,7 @@
 // global bind some event
 // focus, submit
 var globalBindList = 'focus';
-var bindElements = 'form[data-validate="parsley"] input, [data-bind="true"] input, form[data-validate="parsley"] textarea, [data-bind="true"] textarea';
+var bindElements = 'form[data-validate="parsley"] input, [data-bind="true"] input, form[data-validate="parsley"] textarea, [data-bind="true"] textarea, [data-bind="true"]';
 
 var isBind = function( elem ) {
   elem = elem instanceof $ ? elem : $( elem );
@@ -1785,7 +1781,7 @@ var bindForm = function( e ) {
   if ( !isBind( form ) ) {
     form.parsley();
   } else {
-    var inputs = form.find('input[type=text], input[type=radio], input[type=checkbox]');
+    var inputs = form.find('input[type=text], input[type=radio], input[type=checkbox], [contenteditable="true"]');
     inputs.each( function (index) {
       if ( !isBind( this ) ) {
         formAddItem( form, this );
@@ -1833,163 +1829,5 @@ $(document.body).on( globalBindList, bindElements, bindFiled );
 
 
 
-/*
-#**********************************************************
-#* Filename: UI.errortip
-#* Creator: Tim
-#* Description: UI.errortip
-#* Date: 20130817
-# **********************************************************
-# (c) Copyright 2013 Madeiracloud  All Rights Reserved
-# **********************************************************
-*/
-var errortip = function (event)
-{
-  var target = $(this),
-    content = errortip.findError( target )
-    , target_offset
-    , width
-    , height
-    , target_width
-    , target_height
-    , tipId
-
-  if (content.length)
-  {
-    id = content.attr('id');
-    tipId = 'errortip-' + id;
-    originTip = $('#' + tipId);
-
-    if ( originTip.length ) {
-      // if error message is changed, update the errortip and display it.
-      if ( originTip.html() != content.html() ) {
-        originTip.html( content.html() )
-        originTip.show();
-      }
-      return;
-    }
-
-    errortip_box = content.clone();
-    errortip_box
-      .addClass('errortip_box')
-      .attr('id', tipId)
-      .css('z-index', content.css('z-index'))
-      .appendTo(document.body);
-
-    target_offset = target.offset();
-    target_width = target.innerWidth();
-    target_height = target.innerHeight();
-
-    //width = errortip_box.width();
-    //height = errortip_box.height();
-
-
-    errortip_box.css({
-      'left': target_offset.left,
-      'top': target_offset.top + target_height + height - document.body.scrollTop + 45 > window.innerHeight ?
-        target_offset.top - height - 15 + 5 :
-        target_offset.top + target_height + 5,
-      width: target_width - 8 - 2,
-
-    }).show();
-
-    errortip.timer[ id ] = setInterval(function ()
-      {
-        if (content.closest('html').length === 0)
-        {
-          errortip.clear( content.attr('id') );
-        }
-      }, 200);
-
-  }
-};
-
-errortip.timer = {};
-errortip.firstTimer = {};
-errortip.isEnter = false;
-
-errortip.findError = function( $target ) {
-  return $target.next('.parsley-error-list');
-}
-
-errortip.getEid = function ( target ) {
-  $target = target instanceof $ ? target : $( target );
-  return errortip.findError( $target ).attr( 'id' )
-}
-
-errortip.getUid = function ( event ) {
-  var id;
-  if ( event === Object( event ) ) {
-    id = errortip.getEid( event.currentTarget );
-  } else {
-    id = event
-  }
-  return $( '#' + id ).data( 'uid' );
-}
-
-errortip.first = function( target ) {
-  errortip.call(target)
-  id = errortip.getEid( target )
-  errortip.firstTimer[ id ] = setTimeout(function() {
-    errortip.clear({currentTarget: target});
-  }, 2000);
-}
-
-errortip.clear = function ( event )
-{
-  var id, uid, force = false;
-  if ( event ){
-    var errorPrefix = 'errortip-';
-    if ( event === Object( event ) ) {
-      id = errortip.findError( $( event.currentTarget ) ).attr( 'id' );
-    }
-    else {
-      force = true;
-      id = event;
-    }
-
-    uid = errortip.getUid( id );
-    setTimeout( function() {
-      if ( errortip.enterUid !== uid  || force ) {
-        $( '#' + errorPrefix + id ).remove();
-      }
-    }, 100);
-
-  } else {
-    $('.errortip_box').remove();
-  }
-
-  errortip.firstTimer[ id ] && clearInterval( errortip.firstTimer[ id ] )
-  errortip.removeInterval( id )
-
-};
-
-errortip.removeInterval = function ( id ) {
-  if ( id ) {
-    clearInterval( errortip.timer[ id ] );
-  } else {
-    for ( var id in errortip.timer ) {
-      clearInterval( errortip.timer[ id ] );
-    }
-  }
-}
-
-errortip.enter = function ( event ) {
-  errortip.enterUid = errortip.getUid( event );
-  errortip.call( this, event );
-}
-
-errortip.leave = function ( event ) {
-  errortip.enterUid = false;
-  errortip.clear.call( this, event );
-}
-
-$(document).ready(function ()
-{
-  $(document.body).on('mouseenter', '.parsley-error', errortip.enter);
-  $(document.body).on('mouseleave', '.parsley-error', errortip.leave);
-});
-
-Util.errortip = errortip;
 
 }(window.jQuery)

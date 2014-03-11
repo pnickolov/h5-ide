e76a6e241c52a71ca684d45351a9cfbb34fc4e3f
MorrisLiang <morris@mc2.io>
2014-03-11 12:06:14 +0800

    [feature/gulp]: Updated ideversion

--- util/gulp_tasks/plugins/ideversion.js
+++ util/gulp_tasks/plugins/ideversion.js
@@ -6,7 +6,7 @@
   pkgInfo = null;
 
   version = function() {
-    var date;
+    var date, time;
     if (pkgInfo) {
       return pkgInfo.version;
     }
@@ -15,8 +15,15 @@
     }));
     version = pkgInfo.version.split(".");
     version[1] = parseInt(version[1], 10) + 1;
-    pkgInfo.version = version.join(".");
     date = new Date();
+    time = [date.getHours(), date.getMinutes()];
+    if (time[1] < 10) {
+      time[1] = "0" + time[1];
+    }
+    if (time[2] < 10) {
+      time[2] = "0" + time[2];
+    }
+    version[2] = time.join("");
     date = [date.getFullYear(), date.getMonth() + 1, date.getDate()];
     if (date[1] < 10) {
       date[1] = "0" + date[1];
@@ -24,6 +31,7 @@
     if (date[2] < 10) {
       date[2] = "0" + date[2];
     }
+    pkgInfo.version = version.join(".");
     GLOBAL.gulpConfig.version = pkgInfo.version + ("." + (date.join('')));
     return GLOBAL.gulpConfig.version;
   };

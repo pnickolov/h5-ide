144a52838e320ec83e9a0fe625511b852fe01ae7
Paula Meng <0065paula@gmail.com>
2014-06-20 20:38:52 +0800

    [release/publish-v20140619] fix volume bubble style

--- src/ui/MC.template.js
+++ src/ui/MC.template.js
@@ -1165,9 +1165,9 @@ function program4(depth0,data) {
   return "volume";
   }
 
-  buffer += "<div class=\"bubble-head\">Attached Volume <span id=\"instance_volume_number\">("
+  buffer += "<div class=\"bubble-head\">Attached Volume <span id=\"instance_volume_number\">"
     + escapeExpression(((stack1 = (depth0 && depth0.length)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
-    + ")</span></div>\n<div id=\"instance_volume_wrap\" class=\"scroll-wrap\">\n	<div class=\"scrollbar-veritical-wrap\"><div class=\"scrollbar-veritical-thumb\"></div></div>\n	<ul id=\"instance_volume_list\" class=\"clearfix bubble-content scroll-content\">\n		";
+    + "</span></div>\n<div id=\"instance_volume_wrap\" class=\"scroll-wrap\">\n	<div class=\"scrollbar-veritical-wrap\"><div class=\"scrollbar-veritical-thumb\"></div></div>\n	<ul id=\"instance_volume_list\" class=\"clearfix bubble-content scroll-content\">\n		";
   stack1 = helpers.each.call(depth0, depth0, {hash:{},inverse:self.noop,fn:self.program(1, program1, data),data:data});
   if(stack1 || stack1 === 0) { buffer += stack1; }
   buffer += "\n	</ul>\n</div>";

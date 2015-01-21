83db6d68a16ba66300710f44a588c7fe4272530b
Jimmy Xu <xjimmyshcn@gmail.com>
2015-01-21 15:58:26 +0800

    [feature/team-account] update promise api

--- src/api/define/aws/aws.js
+++ src/api/define/aws/aws.js
@@ -5,7 +5,6 @@ define(['ApiRequestDefs'], function( ApiRequestDefs ){
 		'aws_property'       : { type:'aws', url:'/aws/',	method:'property',	params:['username', 'session_id']   },
 		'aws_aws'            : { type:'aws', url:'/aws/',	method:'aws',	params:['username', 'session_id', 'region_names', 'fields', 'filters']   },
 		'aws_resource'       : { type:'aws', url:'/aws/',	method:'resource',	params:['username', 'session_id', 'key_id', 'region_name', 'resources', 'addition', 'retry_times']   },
-		'aws_validate_credential' : { type:'aws', url:'/aws/',	method:'validate_credential',	params:['username', 'session_id', 'account_id', 'access_key', 'secret_key', 'project_id', 'key_id']   },
 	}
 
 	for ( var i in Apis ) {
